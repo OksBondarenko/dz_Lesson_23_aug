9177f5fcb1aec27777daae4f38adc41c64b300f6
Oksana bondarenko <roksanapopova@gmail.com>
2022-08-30 01:03:21 +0200

    Task 41

---
+++ Task41/Program.cs
@@ -0,0 +1,24 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+string[] SplitOnArray(string a)
+{
+    string[] result = a.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+    return result;
+}
+
+int CountPositiveNumbers(string[] num)
+{
+    int count = 0;
+
+    foreach (string i in num)
+    {
+        if (Convert.ToInt32(i) > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine("Введите числа через пробел: ");
+Console.WriteLine($"В введенных числах {CountPositiveNumbers(SplitOnArray(Console.ReadLine()))} положительных значений");
